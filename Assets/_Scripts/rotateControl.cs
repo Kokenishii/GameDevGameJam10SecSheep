b4e9664a7afa97d77a2ecd70ce8db09f8ad5d9df
Unknown <ki575@nyu.edu>
2017-05-01 21:18:28 -0400

    First Test! With Rotation Script

---
+++ Assets/_Scripts/rotateControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateControl : MonoBehaviour {
+    float leftRight;
+    public float moveConstant=1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        leftRight = -Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0,0,leftRight*moveConstant));
+		
+	}
+}
