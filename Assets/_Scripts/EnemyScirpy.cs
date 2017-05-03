e2c6e88efffedf02a4b38694b059a81126cad728
nalrayes <na1487@nyu.edu>
2017-05-03 16:55:29 -0400

    random generation of enimies within the circle

---
+++ Assets/_Scripts/EnemyScirpy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScirpy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject circle = GameObject.Find ("ClockCenter").transform.FindChild ("clockBase").gameObject;
+		float radius = circle.GetComponent<SpriteRenderer> ().bounds.extents.x;
+		transform.position = Random.insideUnitCircle * Random.Range (1, radius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/randomSpawningScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawningScript : MonoBehaviour {
+	int timer;
+	public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {	
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer++;
+		if (timer == 150) {
+			timer = 0;
+			for (int i = 0; i < 10; i++) {
+				GameObject enemy = Instantiate (enemyPrefab) as GameObject;
+			}
+		}
+	}
+}
