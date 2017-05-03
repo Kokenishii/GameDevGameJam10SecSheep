0c730797046722b4c4b70a8895233fc7a26fa593
Unknown <ki575@nyu.edu>
2017-05-03 16:41:35 -0400

    Create the prefab for the projectile! (●'◡'●)

---
+++ Assets/_Scripts/projectileScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileScript : MonoBehaviour {
+    public float projectileSpeed=1;
+    public GameObject clockHandPlayer;
+    public Vector3 shootDirection;
+	// Use this for initialization
+	void Start () {
+        shootDirection = new Vector3(Mathf.Cos(Mathf.Deg2Rad*clockHandPlayer.transform.localEulerAngles.z), 
+                                   Mathf.Sin(Mathf.Deg2Rad * clockHandPlayer.transform.localEulerAngles.z), 
+                                                                                                        0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(clockHandPlayer.transform.localRotation.z);
+        transform.position += projectileSpeed*Time.deltaTime*shootDirection;
+	}
+}
