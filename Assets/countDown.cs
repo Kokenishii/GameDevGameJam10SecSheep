10041e693056d0243074c281c09e3cbfe3683c4a
Unknown <ki575@nyu.edu>
2017-05-06 15:39:52 -0400

    Timer Added

---
+++ Assets/countDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class countDown : MonoBehaviour {
+    float time=10;
+    public Text countdownText;
+    int timeText;
+	// Use this for initialization
+	void Start () {
+        time = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        timeText = Mathf.CeilToInt(time);
+        countdownText.text = timeText.ToString();
+        if (time<=0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        Debug.Log(time);
+    }
+   
+}
